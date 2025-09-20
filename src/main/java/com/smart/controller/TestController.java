23296f19b6507710b87cc8ce1639b55793939bea
Azlan Khatti <azalankhatti786@gmail.com>
2025-09-20 18:24:10 +0500

    Some change.

---
+++ src/main/java/com/smart/controller/TestController.java
@@ -0,0 +1,7 @@
+package com.smart.controller;
+
+public class TestController {
+    String test(){
+        return "Test controller is running.";
+    }
+}
--- src/main/java/com/smart/dao/UserRepository.java
+++ src/main/java/com/smart/dao/UserRepository.java
@@ -5,8 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface UserRepository extends JpaRepository<User,Integer> {
+import java.util.Optional;
 
-        @Query("select u from User u where u.email = :email")
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Query("select u from User u where u.email = :email")
     public User getUserByUserName(@Param("email") String email);
+
+    Optional<User> findUserByEmail(String email);
+
 }
