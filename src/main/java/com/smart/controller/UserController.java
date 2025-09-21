f026296c41a8d63c615bad018f217896a7d32e41
Azlan Khatti <azalankhatti786@gmail.com>
2025-09-21 14:25:28 +0500

    Some working on this project

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -5,14 +5,21 @@ import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
 import com.smart.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
+
+import static org.aspectj.weaver.tools.cache.SimpleCacheFactory.path;
 
 @Controller
 @RequestMapping("/user")
@@ -56,11 +63,33 @@ public class UserController {
 
     //processing add contact form
         @PostMapping("/process-contact")
-        public String processContact(@ModelAttribute Contact contact,Principal principal){
+        public String processContact(@ModelAttribute Contact contact,
+                                     @RequestParam("profileImage") MultipartFile file,
+                                     Principal principal){
 
-
+            try{
             String name =principal.getName();
            User user =  this.userRepository.getUserByUserName(name);
+
+           //processing and uploading file
+
+                if(file.isEmpty()){
+                    //if the file is empty then try our message
+                    System.out.println("File is empty");
+                }else{
+                    //file the file to folder and update the name contact
+                    contact.setImage(file.getOriginalFilename());
+
+                   File saveFile =new ClassPathResource("static/image").getFile();
+
+                   Path path =  Paths.get(saveFile.getAbsolutePath()+File.separator+file.getOriginalFilename());
+
+                    Files.copy(file.getInputStream(),path, StandardCopyOption.REPLACE_EXISTING);
+
+                    System.out.println("image is upload");
+
+
+                }
 
            contact.setUser(user);
             user.getContact().add(contact);
@@ -70,6 +99,16 @@ public class UserController {
             System.out.println("DATA " +contact);
 
             System.out.println("Added to database");
+
+            } catch (Exception e) {
+
+
+                System.out.println("ERROR "+e.getMessage());
+                e.printStackTrace();
+            }
+
+
+
             return "normal/add_contact_form";
         }
 }
